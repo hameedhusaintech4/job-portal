5317ef43e0762b0fc3d0b64cc81119d4b3485699
Easwar M <easwartech4@gmail.com>
2026-04-01 10:38:53 +0530

    Sample

--- resources/views/employer/dashboard.blade.php
+++ resources/views/employer/dashboard.blade.php
@@ -1,8 +1,11 @@
-<form method="POST" action="/login">
-    @csrf
-    <input type="hidden" name="role" value="employee">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-
-    <button>Login</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
